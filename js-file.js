efa585916a8cc8ed9780e147a524bc786c3fa59d
Jared <jaredmgray@outlook.com>
2024-09-25 20:44:25 -0500

    {

--- js-file.js
+++ js-file.js
@@ -1,8 +1,40 @@
 const gameboard = document.querySelector("#gameboard")
-const gridSize = document.querySelectorAll(".gridSize button")
+const gridSize = document.querySelector(".gridSize .slider")
+const rainbowBtn = document.querySelector(".rainbow")
+const eraser = document.querySelector(".eraser")
+const clear = document.querySelector(".clear")
 
-let size = 16;
+const textSize = document.querySelector(".sizeText")
+
+let size = gridSize.value;
 let pxWidth = 500;
+let isRainbow = false;
+let isEraser = false;
+
+rainbowBtn.addEventListener('click', () => {
+    if (rainbowBtn.textContent === "Rainbow: Off"){
+        rainbowBtn.textContent = "Rainbow: On"
+        rainbowBtn.style.backgroundColor = "rgb(209, 209, 111)"
+        isRainbow = true;
+    }else if (rainbowBtn.textContent === "Rainbow: On"){
+        rainbowBtn.textContent = "Rainbow: Off"
+        rainbowBtn.style.backgroundColor = "rgb(235, 235, 127)"
+        isRainbow = false;
+    }
+})
+
+eraser.addEventListener('click', () => {
+    if (eraser.textContent === "Eraser: Off"){
+        eraser.textContent = "Eraser: On"
+        eraser.style.backgroundColor = "rgb(209, 209, 111)"
+        isEraser = true
+    }else if (eraser.textContent === "Eraser: On"){
+        eraser.textContent = "Eraser: Off"
+        eraser.style.backgroundColor = "rgb(235, 235, 127)"
+        isEraser = false
+    }
+})
+
 
 function createGrid (size){
     for (let row = 0; row < size; row++) {
@@ -25,7 +57,13 @@ function createGrid (size){
 
     box.forEach((div) => {
         div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = "black"
+        if (isEraser === true){
+            div.style.backgroundColor = "#eeeeee"
+        }else if (isRainbow === true){
+            div.style.backgroundColor = randomColor()
+        }else if (isRainbow === false){
+            div.style.backgroundColor = "black"
+        }
         
     })
 })
@@ -33,12 +71,22 @@ function createGrid (size){
     
 createGrid(size)
 
-
-gridSize.forEach((button) => {
-    button.addEventListener('click', () => {
-        gameboard.innerHTML = ""
-        size = button.value
-        createGrid(size)
-        
+clear.addEventListener('click', () => {
+    gameboard.innerHTML = ""
+    createGrid(size)
 })
-});
+gridSize.addEventListener("input", () => {
+    size = gridSize.value;
+    gameboard.innerHTML = ""
+    createGrid(size)
+    textSize.textContent = `${size} x ${size}`
+})
+
+function randomColor (){
+    const letter = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++){
+        color += letter[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
